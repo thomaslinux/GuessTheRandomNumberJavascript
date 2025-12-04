36e651367dc2f735d2a782da57bc0f79842fa287
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2025-12-04 22:07:47 +0100

    Update random number range and input validation
    Change random number range to start from 0 and ensure input only contains numbers.

---
+++ script.js
@@ -0,0 +1,48 @@
+output = document.getElementById("output");
+input = document.getElementById("input");
+
+nbToFind = alea(0,100);
+verif();
+input.addEventListener("wheel",onWheel)
+input.addEventListener("input",verif)
+
+function onWheel(e) {
+    e.preventDefault();
+    if (e.deltaY > 0) { // on scroll down
+        decreaseInput();
+    } else if (e.deltaY < 0) { // on scroll up
+        increaseInput();
+    }
+    verif();
+}
+
+function increaseInput() {
+    input.value++;
+}
+
+function decreaseInput() {
+    input.value--;
+}
+
+function verif() {
+    input.value = input.value.replace(/\D+/g, ''); // force numbers
+    if (input.value === '') {
+        // input.value = '0'
+        output.innerText = "Find the number :"
+    } else if (input.value > nbToFind) {
+        output.innerText = "Number to find is less than " + input.value;
+    } else if (input.value < nbToFind) {
+        output.innerText = "Number to find is more than " + input.value;
+    } else {
+        output.innerText = "You guessed right !"
+    }
+}
+
+/**
+ * @param {Number} MIN valeur minimum du nombre aléatoire (inclue)
+ * @param {Number} MAX valeur maximum du nombre aléatoire (excluse)
+ * @returns {Number} un entier aléatoire entre une valeur MIN (inclue) et une valeur MAX (exclue)
+ */
+function alea(MIN,MAX) {
+    return Math.floor(Math.random()*(MAX-MIN)+ MIN);
+}
