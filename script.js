d989764ae7fb93f775b610c73c9f418dc1835b69
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2025-12-09 08:58:22 +0000

    wheelUp wheelDown useful at last, format code for better reading

--- script.js
+++ script.js
@@ -8,32 +8,32 @@ const infoNbVerif = document.getElementById("nbVerif");
 const htmlMIN = document.getElementById("min");
 const htmlMAX = document.getElementById("max");
 const reload = document.getElementById("reload");
-reload.addEventListener('click', (e) => {init();});
-window.onload=init;
+reload.addEventListener('click', (e) => { init(); });
+window.onload = init;
 
-function init () {
+function init() {
     htmlMIN.innerText = MIN;
     htmlMAX.innerText = MAX;
-    nbToFind = alea(MIN,MAX);
+    nbToFind = alea(MIN, MAX);
     nbVerif = -1;
     verif();
     input.value = MAX;
     verifNb();
     infoNbVerif.innerText = "0";
-    input.addEventListener("input",verif)
-    input.addEventListener("wheel",onWheel)
-    function onWheel(e) {
-    e.preventDefault();
-    if (e.deltaY > 0) { // on scroll down
-        decreaseInput();
-    } else if (e.deltaY < 0) { // on scroll up
-        increaseInput();
-    }
-    
-}
 }
 
-document.addEventListener('keydown', function(e) {
+input.addEventListener("input", verif)
+input.addEventListener("wheel", onWheel)
+function onWheel(e) {
+    e.preventDefault();
+    if (e.deltaY > 0) { // on scroll down
+        operation('-',MAX/20);
+    } else if (e.deltaY < 0) { // on scroll up
+        operation('+',MAX/20);
+    }
+}
+
+document.addEventListener('keydown', function (e) {
     verifKey(e);
 });
 function verifKey(e) {
@@ -42,13 +42,13 @@ function verifKey(e) {
     }
     //if (e.key === 'ArrowUp') {operation('+',1);}
     if (e.key === 'ArrowRight') {
-        operation('+',MAX/20);
+        operation('+', MAX / 20);
     }
     //if (e.key === 'ArrowDown') {operation('-',1);}
-     if (e.key === 'ArrowLeft') {
-        operation('-',MAX/20);
+    if (e.key === 'ArrowLeft') {
+        operation('-', MAX / 20);
     }
-    
+
 }
 
 
@@ -57,11 +57,11 @@ function operation(operator, number) {
     let value = parseInt(input.value) || 0;
     let result = 0;
     switch (operator) {
-        case '*' : result = value * number; break;
-        case '+' : result = value + number; break;
-        case '-' : result = value - number; break;
-        case '/' : value != 0 ? result = value / number : result = value; break;
-        default : result = value; break;
+        case '*': result = value * number; break;
+        case '+': result = value + number; break;
+        case '-': result = value - number; break;
+        case '/': value != 0 ? result = value / number : result = value; break;
+        default: result = value; break;
     }
     input.value = Math.floor(result);
     verifNb();
@@ -92,6 +92,6 @@ function verifNb() {
  * @param {Number} MAX valeur maximum du nombre aléatoire (excluse)
  * @returns {Number} un entier aléatoire entre une valeur MIN (inclue) et une valeur MAX (exclue)
  */
-function alea(MIN,MAX) {
-    return Math.floor(Math.random()*(MAX-MIN)+ MIN);
+function alea(MIN, MAX) {
+    return Math.floor(Math.random() * (MAX - MIN) + MIN);
 }
