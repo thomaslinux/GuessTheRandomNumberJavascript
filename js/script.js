6496ea94808551a864c9f80592688442012dae55
thomaslinux <182340067+thomaslinux@users.noreply.github.com>
2025-12-17 11:01:58 +0100

    UPPER_SNAKE_CASE on constants

--- js/script.js
+++ js/script.js
@@ -4,33 +4,33 @@ let nbToFind = 0;
 let nbVerif = 0;
 let wins = 0;
 let boolWin = false;
-const output = document.getElementById("output");
-const input = document.getElementById("input");
-const infoNbVerif = document.getElementById("nbVerif");
-const htmlMIN = document.getElementById("min");
-const htmlMAX = document.getElementById("max");
-const reload = document.getElementById("reload");
+const HTML_OUTPUT = document.getElementById("output");
+const HTML_INPUT = document.getElementById("input");
+const HTML_INFO_NB_VERIF = document.getElementById("nbVerif");
+const HTML_MIN = document.getElementById("min");
+const HTML_MAX = document.getElementById("max");
+const HTML_RELOAD_BUTTON = document.getElementById("reload");
 document.querySelector("#version").innerText += "\nJS_2025-12-17_10:44"
-reload.addEventListener('click', (e) => { init(); });
+HTML_RELOAD_BUTTON.addEventListener('click', (e) => { init(); });
 // window.onload = init;
 init();
 
 function init() {
     wins = parseInt(localStorage.getItem("wins")) || 0;
     boolWin = false;
-    htmlMIN.innerText = MIN;
-    htmlMAX.innerText = MAX;
+    HTML_MIN.innerText = MIN;
+    HTML_MAX.innerText = MAX;
     nbToFind = alea(MIN, MAX);
     nbVerif = -1;
     verif();
-    input.value = MAX;
+    HTML_INPUT.value = MAX;
     verifNb();
-    infoNbVerif.innerText = "0";
+    HTML_INFO_NB_VERIF.innerText = "0";
     document.querySelector("#wins span").innerText = wins;
 }
 
-input.addEventListener("input", verif)
-input.addEventListener("wheel", onWheel)
+HTML_INPUT.addEventListener("input", verif)
+HTML_INPUT.addEventListener("wheel", onWheel)
 function onWheel(e) {
     e.preventDefault();
     if (e.deltaY > 0) { // on scroll down
@@ -44,7 +44,7 @@ function onWheel(e) {
 // Already on keydown so why ??
 document.addEventListener('keydown', function (e) {  verifKey(e); });
 // input.keydown(function (e) {  verifKey(e); }) // jQuery only also input already exists ??
-input.addEventListener('keydown', function (e) {  verifKey(e); });
+HTML_INPUT.addEventListener('keydown', function (e) {  verifKey(e); });
 function verifKey(e) {
     if (e.key === 'Enter' || e.key === 'Tab' || event.keyCode === 9) { e.preventDefault(); verifNb(); }
     if (e.key === 'n' || e.key === 'c' || e.key === '.') { e.preventDefault(); init(); }
@@ -53,10 +53,10 @@ function verifKey(e) {
     if (e.key === 'ArrowDown') { e.preventDefault(); operation('-', MAX / 20); }
     //if (e.key === 'ArrowLeft') {e.preventDefault(); operation('-', MAX / 20);}
     if (e.key === '-' || e.key === 'q') {
-        e.preventDefault(); operation('-', (htmlMAX.innerText-htmlMIN.innerText)/2);
+        e.preventDefault(); operation('-', (HTML_MAX.innerText-HTML_MIN.innerText)/2);
     } // dichotomic substraction
     if (e.key === '+' || e.key === 's' || e.key === ' ') {
-        e.preventDefault(); operation('+', (htmlMAX.innerText-htmlMIN.innerText)/2);
+        e.preventDefault(); operation('+', (HTML_MAX.innerText-HTML_MIN.innerText)/2);
     } // dichotomic addition
     if (e.key === 'a') { e.preventDefault(); operation('/', 2); }
     if (e.key === 'z') { e.preventDefault(); operation('*', 1.5); }
@@ -68,7 +68,7 @@ function verifKey(e) {
 
 function operation(operator, number) {
     if (boolWin === false) {
-    let value = parseInt(input.value) || 0;
+    let value = parseInt(HTML_INPUT.value) || 0;
     let result = 0;
     switch (operator) {
         case '*': result = value * number; break;
@@ -77,13 +77,13 @@ function operation(operator, number) {
         case '/': value != 0 ? result = value / number : result = value; break;
         default: result = value; break;
     }
-    input.value = Math.floor(result);
+    HTML_INPUT.value = Math.floor(result);
     verifNb();
     }
 }
 
 function verif() {
-    input.value = input.value.replace(/\D+/g, ''); // force numbers
+    HTML_INPUT.value = HTML_INPUT.value.replace(/\D+/g, ''); // force numbers
 }
 function verifNb() {
 
@@ -94,25 +94,25 @@ function verifNb() {
     document.querySelector("#indicator_win").style.display = "none";
 
     // display the element to display
-    if (input.value === '') {
+    if (HTML_INPUT.value === '') {
         // input.value = '0'
-        output.innerText = "Guess the number !"
+        HTML_OUTPUT.innerText = "Guess the number !"
         document.querySelector("#indicator_empty").style.display = "block";
-    } else if (nbToFind > input.value) {
-        output.innerText = "Number > " + input.value;
+    } else if (nbToFind > HTML_INPUT.value) {
+        HTML_OUTPUT.innerText = "Number > " + HTML_INPUT.value;
         document.querySelector("#indicator_more").style.display = "block";
-        document.querySelector("#indicator_more span").innerText = input.value;
-        htmlMIN.innerText = input.value > MIN ? input.value : MIN;
-    } else if (nbToFind < input.value) {
-        output.innerText = "Number < " + input.value;
+        document.querySelector("#indicator_more span").innerText = HTML_INPUT.value;
+        HTML_MIN.innerText = HTML_INPUT.value > MIN ? HTML_INPUT.value : MIN;
+    } else if (nbToFind < HTML_INPUT.value) {
+        HTML_OUTPUT.innerText = "Number < " + HTML_INPUT.value;
         document.querySelector("#indicator_less").style.display = "block";
-        document.querySelector("#indicator_less span").innerText = input.value;
-        htmlMAX.innerText = input.value < MAX ? input.value : MAX;
+        document.querySelector("#indicator_less span").innerText = HTML_INPUT.value;
+        HTML_MAX.innerText = HTML_INPUT.value < MAX ? HTML_INPUT.value : MAX;
     } else {
-        output.innerText = "You win !"
+        HTML_OUTPUT.innerText = "You win !"
         document.querySelector("#indicator_win").style.display = "block";
-        htmlMIN.innerText = input.value;
-        htmlMAX.innerText = input.value;
+        HTML_MIN.innerText = HTML_INPUT.value;
+        HTML_MAX.innerText = HTML_INPUT.value;
         wins = document.querySelector("#wins span").innerText;
         if (boolWin === false) {
         wins++;
@@ -123,7 +123,7 @@ function verifNb() {
     }
     if (boolWin === false) { // stop verif counter when win
     nbVerif++;
-    infoNbVerif.innerText = nbVerif;
+    HTML_INFO_NB_VERIF.innerText = nbVerif;
     }
 }
 
